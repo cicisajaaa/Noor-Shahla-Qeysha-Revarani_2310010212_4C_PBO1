d3044c30761f6d2b827ac0a27c00e9fb517fc6f7
cicisajaaa <shahlaqeysha@gmail.com>
2025-07-02 12:32:00 +0800

    Menyempurnakan code di class User

--- src/reminderminumair/User.java
+++ src/reminderminumair/User.java
@@ -3,5 +3,17 @@ package reminderminumair;
 
 
 public class User {
+    protected String nama;
     
+    public User(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public void tampilkanProfil(){
+        System.out.println("Nama Pengguna: " + nama);
+    }
 }
