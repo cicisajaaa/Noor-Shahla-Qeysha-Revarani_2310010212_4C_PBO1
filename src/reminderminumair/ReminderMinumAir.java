54d866aac1da0188ccd7f0d534e18046676fe233
cicisajaaa <shahlaqeysha@gmail.com>
2025-07-02 10:35:01 +0800

    Menginisialisasi aplikasi pengingat minum air

--- src/reminderminumair/ReminderMinumAir.java
+++ src/reminderminumair/ReminderMinumAir.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package reminderminumair;
 
-/**
- *
- * @author ASUS
- */
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class ReminderMinumAir {
-    
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            LocalDate hariIni = LocalDate.now();
+            
+            System.out.println("====================");
+            System.out.println("APLIKASI PENGINGAT MINUM AIR SEDERHANA");
+            System.out.println("====================");
+            
+            System.out.println("Masukkan nama Anda: ");
+            String nama = input.nextLine();
+            
+            System.out.println("Masukkan Target Minum Harian Anda (ml) : ");
+            int target = input.nextInt();
+        }
+    }
+ 
 }
