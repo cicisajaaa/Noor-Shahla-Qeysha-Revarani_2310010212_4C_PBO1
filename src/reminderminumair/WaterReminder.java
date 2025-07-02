5492b1c795280dc487e5123a8bc1b31e47bae5e3
cicisajaaa <shahlaqeysha@gmail.com>
2025-07-02 11:23:19 +0800

    Membuat atribut, konstruktor, dan accessor

--- src/reminderminumair/WaterReminder.java
+++ src/reminderminumair/WaterReminder.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package reminderminumair;
 
-/**
- *
- * @author ASUS
- */
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class WaterReminder {
-    
+    private final String namaPengguna;                  // Nama pengguna
+    private final int targetHarian;                     // Target minum harian dalam ml
+    private final Map<LocalDate, Integer> catatanAir;   // Catatan jumlah minum per tanggal
+
+    public WaterReminder(String nama, int target) {
+        this.namaPengguna = nama;
+        this.targetHarian = target;
+        this.catatanAir = new HashMap<>();
+    }
+
+    public String getNamaPengguna() {
+        return namaPengguna;
+    }
+
+    public int getTargetHarian() {
+        return targetHarian;
+    }
+
 }
