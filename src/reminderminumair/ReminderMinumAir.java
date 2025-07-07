042fdb7d9ed248dcc94858b6a68a8b0a53e9d7b1
cicisajaaa <shahlaqeysha@gmail.com>
2025-07-07 17:40:38 +0800

    Melengkapi code di dalam Error Handling

--- src/reminderminumair/ReminderMinumAir.java
+++ src/reminderminumair/ReminderMinumAir.java
@@ -3,6 +3,9 @@ package reminderminumair;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
+
 
 public class ReminderMinumAir {
     public static void main(String[] args) {
@@ -22,8 +25,18 @@ public class ReminderMinumAir {
             System.out.println("Masukkan nama Anda: ");
             String nama = input.nextLine();
             
-            System.out.println("Masukkan Target Minum Harian Anda (ml) : ");
-            int target = input.nextInt();
+           int target = 0;
+            while (true) {
+                System.out.print("Masukkan Target Minum Harian Anda (ml): ");
+                try {
+                    target = input.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println(" Input harus berupa angka!");
+                    input.nextLine(); 
+                }
+            }
+
             
             pengguna[jumlahPengguna] = new WaterReminder(nama, target);
             WaterReminder reminder = pengguna[jumlahPengguna];
@@ -43,42 +56,78 @@ public class ReminderMinumAir {
                 System.out.println("5. Lihat Riwayat Minum");
                 System.out.println("6. Ubah Target Harian");
                 System.out.println("0. Keluar");
-                System.out.print("Pilihan: ");
-                pilihan = input.nextInt();
+                try {
+                    System.out.print("Pilihan: ");
+                    pilihan = input.nextInt();
+                    input.nextLine(); 
+                } catch (InputMismatchException e) {
+                    System.out.println("Input harus berupa angka!");
+                    input.nextLine(); 
+                    continue;
+                }
+
                 
                 switch (pilihan) {
                     case 1 -> {
-                        System.out.println("Masukkan Jumlah Air (ml): ");
-                        int jumlah = input.nextInt();
-                        reminder.tambahAir(hariIni, jumlah);
+                        try {
+                            System.out.print("Masukkan Jumlah Air (ml): ");
+                            int jumlah = input.nextInt();
+                            reminder.tambahAir(hariIni, jumlah);
+                        } catch (InputMismatchException e) {
+                            System.out.println("Input tidak valid. Harus angka.");
+                            input.nextLine();
+                        }
                     }
+
                     case 2 -> 
                         reminder.lihatProgress(hariIni);
                     case 3 -> {
-                        input.nextLine();
-                        System.out.println("Masukkan Tanggal (yyyy-mm-dd): ");
-                        String tglInput = input.nextLine();
-                        LocalDate tanggalLain = LocalDate.parse(tglInput);
-                        System.out.println("Jumlah Air (ml): ");
-                        int jml = input.nextInt();
-                      reminder.tambahAir(tanggalLain, jml);
+                        try {
+                            System.out.print("Masukkan Tanggal (yyyy-mm-dd): ");
+                            String tglInput = input.nextLine();
+                            LocalDate tanggalLain = LocalDate.parse(tglInput);
+
+                            System.out.print("Jumlah Air (ml): ");
+                            int jml = input.nextInt();
+                            reminder.tambahAir(tanggalLain, jml);
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Format tanggal salah. Gunakan yyyy-mm-dd.");
+                        } catch (InputMismatchException e) {
+                            System.out.println("Jumlah air harus berupa angka.");
+                            input.nextLine();
+                        }
                     }
-                    case 4 -> { 
-                        input.nextLine();
-                        System.out.println("Masukkan Tanggal yang ingin di edit (yyyy-mm-dd): ");
-                        String editTgl = input.nextLine();
-                        LocalDate editTanggal = LocalDate.parse(editTgl);
-                        System.out.println("Jumlah air baru (ml): ");
-                        int baru = input.nextInt();
-                        reminder.editAir(editTanggal, baru);
+
+                    case 4 -> {
+                        try {
+                            System.out.print("Masukkan Tanggal yang ingin diedit (yyyy-mm-dd): ");
+                            String editTgl = input.nextLine();
+                            LocalDate editTanggal = LocalDate.parse(editTgl);
+
+                            System.out.print("Jumlah air baru (ml): ");
+                            int baru = input.nextInt();
+                            reminder.editAir(editTanggal, baru);
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Format tanggal salah. Gunakan yyyy-mm-dd.");
+                        } catch (InputMismatchException e) {
+                            System.out.println("Jumlah air harus berupa angka.");
+                            input.nextLine();
+                        }
                     }
+
                     case 5 -> 
                        reminder.lihatRiwayat();
                     case 6 -> {
-                       System.out.println("Masukkan Target Harian Baru (ml): "); 
-                       int targetBaru = input.nextInt();
-                       reminder.setTargetHarian(targetBaru);
+                        try {
+                            System.out.print("Masukkan Target Harian Baru (ml): ");
+                            int targetBaru = input.nextInt();
+                            reminder.setTargetHarian(targetBaru);
+                        } catch (InputMismatchException e) {
+                            System.out.println("Input harus berupa angka.");
+                            input.nextLine();
+                        }
                     }
+
                     case 0 ->
                         System.out.println("Terimakasih sudah menggunakan aplikasi ini.");
                     default ->
